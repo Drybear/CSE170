da72f0980b28fb7f5c283f51f73b37fb46c7f2ee
Gregory Newbrough <gnewbrou@ucsd.edu>
2014-02-28 19:09:55 -0800

    More Google tracking

--- app/views/calendar3.blade.php
+++ app/views/calendar3.blade.php
@@ -26,6 +26,16 @@
 		<script src="{{ asset('js/restfulizer.js') }}"></script>
         <script src="{{ asset('js/fullcalendar.js') }}"></script>		
 		<script src="{{ asset('js/gcal.js') }}"></script>
+			<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-48540804-1', 'memdar.herokuapp.com');
+		  ga('send', 'pageview');
+
+		</script>
 		<!--   -->
 
 		<style>
